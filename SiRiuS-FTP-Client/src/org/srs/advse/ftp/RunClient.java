c7f1ceb5613d5868761d67b5f387e424d760e7aa
Subin Babu <subin.babu@mavs.uta.edu>
2016-12-02 17:15:40 -0600

    Telnet authentication
    Signed-off-by: Subin Babu <subin.babu@mavs.uta.edu>

--- SiRiuS-FTP-Client/src/org/srs/advse/ftp/RunClient.java
+++ SiRiuS-FTP-Client/src/org/srs/advse/ftp/RunClient.java
@@ -3,8 +3,14 @@
  */
 package org.srs.advse.ftp;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 import org.srs.advse.ftp.client.SRSFTPClient;
 import org.srs.advse.ftp.commhandler.ClientCommunicationHandler;
@@ -18,10 +24,22 @@ public class RunClient {
 	public static String hostname;
 	public static String clientDir;
 
+	private static Socket telnetSocket;
+	private static DataInputStream telnetDataInputStream;
+	private static DataOutputStream telnetDataOutputStream;
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+		String ftpPath = System.getProperty("user.home") + File.separator + "ftp";
+
+		System.out.println("Enter your username : ");
+		String username = scanner.nextLine();
+		
 
 		try {
 			hostname = args[0];
@@ -35,8 +53,30 @@ public class RunClient {
 		clientDir = args[3];
 
 		try {
+			boolean logIN = false;
+			telnetSocket = new Socket(hostname, 23);
+			telnetDataInputStream = new DataInputStream(telnetSocket.getInputStream());
+			telnetDataOutputStream = new DataOutputStream(telnetSocket.getOutputStream());
+
+			while (logIN==false) {
+				System.out.println("Enter your password : ");
+				String password = scanner.nextLine();
+				String telnet_user_string = "telnetd_" + username + "_" + password;
+				telnetDataOutputStream.writeUTF(telnet_user_string);
+				String telOutput = telnetDataInputStream.readUTF();
+				logIN = Boolean.parseBoolean(telOutput);
+				if(logIN){
+					System.out.println("Login successful, Welcome User");
+				}else{
+					System.out.println("incorrect password, Try again.");
+				}
+			}
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		try {
 			SRSFTPClient client = new SRSFTPClient();
-			(new Thread(new ClientCommunicationHandler(client, hostname, nPort,clientDir))).start();
+			(new Thread(new ClientCommunicationHandler(client, hostname, nPort, clientDir))).start();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
